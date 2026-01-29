e2e15d925408fa7e9aa7572808fd17d3424acbfb
Thavanish brijesh <thavanishbrijesh@gmail.com>
2026-01-29 20:16:48 +0530

    wtf bro claude made that spinner so shitty

--- airlink/installer.sh
+++ airlink/installer.sh
@@ -27,7 +27,7 @@ show_loading() {
     local i=0
     while kill -0 $pid 2>/dev/null; do
         i=$(( (i+1) %4 ))
-        printf "\r$message ${spin:$i:1}"
+        printf "\r ${spin:$i:1}"
         sleep .1
     done
     printf "\rDone!\n"
